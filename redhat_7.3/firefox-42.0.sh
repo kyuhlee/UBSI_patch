57c934fd6da939165caeef5664c37c8cb0af877e
kyuhlee <kyuhlee@localhost.localdomain>
2017-06-29 18:10:22 -0400

    firefox /usr/local/

--- redhat_7.3/firefox-42.0.sh
+++ redhat_7.3/firefox-42.0.sh
@@ -4,10 +4,14 @@ cd $HOME/UBSI_patch
 wget https://archive.mozilla.org/pub/firefox/releases/42.0/source/firefox-42.0.source.tar.xz
 tar xf firefox-42.0.source.tar.xz
 cd firefox-42.0 && patch -p1 < ../firefox-42.0.patch
-mkdir firefox-build
+cd ..
+sudo mv ./firefox-42.0 /usr/local/
+sudo chown -R root:root /usr/local/firefox-42.0/
+cd /usr/local/firefox-42.0/
+sudo mkdir firefox-build
 cd firefox-build
-$HOME/UBSI_patch/firefox-42.0/configure
-make
+sudo /usr/local/firefox-42.0/configure
+sudo make
 #cd ..
 #sudo mv ./firefox-42.0 /usr/local/
 #sudo chown -R root:root /usr/local/firefox-42.0/
@@ -16,7 +20,6 @@ make
 #sudo make
 unset DISPLAY
 export $(dbus-launch)
-#sudo ln -f -s /usr/local/firefox-42.0/firefox-build/dist/bin/firefox /usr/local/bin/
-sudo ln -f -s $HOME/UBSI_patch/firefox-42.0/firefox-build/dist/bin/firefox /usr/local/bin/
-sudo ln -f -s $HOME/UBSI_patch/firefox-42.0/firefox-build/dist/bin/firefox $HOME/TRACE/BIN/
+sudo ln -f -s /usr/local/firefox-42.0/firefox-build/dist/bin/firefox /usr/local/bin/
+sudo ln -f -s /usr/local/firefox-42.0/firefox-build/dist/bin/firefox $HOME/TRACE/BIN/
 
