0f10fdd92a024197e391e6ebaac058c4b8b48a1f
Kyu <kyuhlee@uga.edu>
2017-12-07 14:48:24 -0500

    fix

--- install.sh
+++ install.sh
@@ -221,7 +221,11 @@ cd $HOME/UBSI_patch
 wget https://ftp.mozilla.org/pub/firefox/releases/54.0.1/source/firefox-54.0.1.source.tar.xz
 tar xf firefox-54.0.1.source.tar.xz
 cd firefox-54.0.1 && patch -p1 < ../firefox-54.0.1.patch
-mkdir src && cd src
-../mach bootstrap
-../mach build
-ln -f -s $HOME/UBSI_patch/firefox-54.0.1/obj-x86_64-pc-linux-gnu/dist/bin/ $HOME/TRACE/BIN/
+cd ..
+sudo mv ./firefox-54.0.1 /usr/local/
+sudo chown -R root:root /usr/local/firefox-54.0.1/
+cd /usr/local/firefox-54.0.1/
+sudo mkdir src && cd src
+sudo ../mach bootstrap
+sudo ../mach build
+ln -f -s /usr/local/firefox-54.0.1/obj-x86_64-pc-linux-gnu/dist/bin/firefox $HOME/TRACE/BIN/
