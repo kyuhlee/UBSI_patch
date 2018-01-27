1d2793ef9a30bf3990f5ce3e96941a27ab86f47e
Kyu <kyuhyung.lee@gmail.com>
2018-01-26 20:59:35 -0500

    Firefox-54.0.1 install without user interaction

--- firefox-54.0.1.sh
+++ firefox-54.0.1.sh
@@ -22,3 +22,16 @@ sudo mkdir src && cd src
 sudo ../mach bootstrap
 sudo ../mach build
 ln -f -s /usr/local/firefox-54.0.1/obj-x86_64-pc-linux-gnu/dist/bin/firefox $HOME/TRACE/BIN/
+
+
+
+sudo apt-get -y install  automake1.4 libossp-uuid16 libpython-dev libpython2.7 libpython2.7-dev libpython2.7-minimal libpython2.7-stdlib python-chardet-whl python-colorama python-colorama-whl python-distlib python-distlib-whl python-html5lib python-html5lib-whl python-pip-whl python-requests-whl python-setuptools-whl python-six-whl python-urllib3-whl python-wheel python2.7 python2.7-dev python2.7-minimal \
+autoconf2.13 ccache python-dev python-pip python-setuptools uuid \
+libatk-bridge2.0-dev libgtk-3-dev libiw-dev libnotify-dev libwayland-dev libx11-xcb-dev libxkbcommon-dev xvfb
+
+
+sudo add-apt-repository -y ppa:mercurial-ppa/releases
+sudo apt-get update
+sudo apt-get install -y mercurial
+
+mkdir $HOME/.mozbuild
