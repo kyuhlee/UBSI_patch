4d2f9169747ce9e7be60da361256edbc5fb53479
Kyu <kyuhyung.lee@gmail.com>
2018-01-26 21:04:21 -0500

    fix broken urls mod script to check the file and download if a file is not exist

---
+++ download.sh
@@ -0,0 +1,45 @@
+if [ ! -f ./httpd-2.4.20.tar.gz ]; then
+		wget https://archive.apache.org/dist/httpd/httpd-2.4.20.tar.gz
+fi
+
+if [ ! -f ./vim-7.3.tar.bz2 ]; then
+		wget ftp://ftp.vim.org/pub/vim/unix/vim-7.3.tar.bz2
+fi
+
+if [ ! -f ./proftpd-1.3.4.tar.gz ]; then
+		wget https://github.com/downloads/proftpd/proftpd.github.com/proftpd-1.3.4.tar.gz
+fi
+
+if [ ! -f ./wget-1.17.1.tar.gz ]; then
+		wget http://ftp.gnu.org/gnu/wget/wget-1.17.1.tar.gz
+fi
+if [ ! -f ./w3m-0.5.3.tar.gz ]; then
+		wget http://downloads.sourceforge.net/project/w3m/w3m/w3m-0.5.3/w3m-0.5.3.tar.gz
+fi
+if [ ! -f ./pine.tar.gz ]; then
+		wget https://www.mirrorservice.org/sites/ftp.cac.washington.edu/pine/pine.tar.gz
+fi
+if [ ! -f ./cherokee-1.2.1.tar.gz ]; then
+		wget http://repository.timesys.com/buildsources/c/cherokee/cherokee-1.2.1/cherokee-1.2.1.tar.gz
+fi
+if [ ! -f ./yafc-1.1.1.tar.gz ]; then
+		wget http://downloads.sourceforge.net/project/yafc/yafc/yafc-1.1.1/yafc-1.1.1.tar.gz
+fi
+if [ ! -f ./transmission-2.60.tar.bz2 ]; then
+		wget https://github.com/transmission/transmission-releases/raw/master/transmission-2.60.tar.bz2
+fi
+if [ ! -f ./bash-4.3.30.tar.gz ]; then
+		wget http://ftp.gnu.org/gnu/bash/bash-4.3.30.tar.gz
+fi
+if [ ! -f ./mc-4.6.1.tar.gz ]; then
+		wget http://ftp.midnight-commander.org/mc-4.6.1.tar.gz
+fi
+if [ ! -f ./openssh-7.2p2.tar.gz ]; then
+		wget http://ftp.vim.org/security/OpenSSH/openssh-7.2p2.tar.gz
+fi
+if [ ! -f ./sendmail.8.15.2.tar.gz ]; then
+		wget ftp://ftp.sendmail.org/pub/sendmail/sendmail.8.15.2.tar.gz
+fi
+if [ ! -f ./firefox-54.0.1.source.tar.xz ]; then
+		wget https://ftp.mozilla.org/pub/firefox/releases/54.0.1/source/firefox-54.0.1.source.tar.xz
+fi
--- install.sh
+++ install.sh
@@ -21,7 +21,9 @@ sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.9 50
 
 # httpd-2.4.20:
 cd /home/$user/UBSI_patch
-#wget https://archive.apache.org/dist/httpd/httpd-2.4.20.tar.gz
+if [ ! -f ./httpd-2.4.20.tar.gz ]; then
+		wget https://archive.apache.org/dist/httpd/httpd-2.4.20.tar.gz
+fi
 tar xzvf httpd-2.4.20.tar.gz
 cd httpd-2.4.20 && patch -p1 < ../httpd-2.4.20.patch
 ./configure -with-mpm=worker && make 
@@ -32,7 +34,9 @@ sudo cp -f /home/$user/UBSI_patch/httpd.conf /usr/local/apache2/conf/httpd.conf
 
 #vim-7.3:
 cd /home/$user/UBSI_patch
-#wget ftp://ftp.vim.org/pub/vim/unix/vim-7.3.tar.bz2
+if [ ! -f ./vim-7.3.tar.bz2 ]; then
+		wget ftp://ftp.vim.org/pub/vim/unix/vim-7.3.tar.bz2
+fi
 bunzip2 vim-7.3.tar.bz2 && tar xvf vim-7.3.tar
 cd vim73 && patch -p1 < ../vim73.patch
 ./configure && make
@@ -41,7 +45,9 @@ sudo cp -f src/vim /usr/local/bin
 
 #Proftpd-1.3.4:
 cd /home/$user/UBSI_patch
-#wget https://ftp.heanet.ie/mirrors/ftp.proftpd.org/distrib/source/proftpd-1.3.4.tar.gz
+if [ ! -f ./proftpd-1.3.4.tar.gz ]; then
+		wget https://github.com/downloads/proftpd/proftpd.github.com/proftpd-1.3.4.tar.gz
+fi
 tar xzvf proftpd-1.3.4.tar.gz
 cd proftpd-1.3.4 && patch -p1 < ../proftpd-1.3.4.patch
 ./configure && make
@@ -51,7 +57,9 @@ sudo cp -f /usr/local/sbin/proftpd /usr/local/bin
 
 #wget-1.17.1:
 cd /home/$user/UBSI_patch
-#wget http://ftp.gnu.org/gnu/wget/wget-1.17.1.tar.gz
+if [ ! -f ./wget-1.17.1.tar.gz ]; then
+		wget http://ftp.gnu.org/gnu/wget/wget-1.17.1.tar.gz
+fi
 tar xzvf wget-1.17.1.tar.gz
 cd wget-1.17.1 && patch -p1 < ../wget-1.17.1.patch
 ./configure && make
@@ -59,7 +67,9 @@ sudo cp -f src/wget /usr/local/bin
 
 #w3m-0.5.3
 cd /home/$user/UBSI_patch
-#wget http://downloads.sourceforge.net/project/w3m/w3m/w3m-0.5.3/w3m-0.5.3.tar.gz
+if [ ! -f ./w3m-0.5.3.tar.gz ]; then
+		wget http://downloads.sourceforge.net/project/w3m/w3m/w3m-0.5.3/w3m-0.5.3.tar.gz
+fi
 tar xzvf w3m-0.5.3.tar.gz
 cd w3m-0.5.3 && patch -p2 < ../w3m-0.5.3.patch
 ./configure LIBS="-lX11 -ldl -lXext -lz" && make
@@ -67,7 +77,9 @@ sudo cp -f ./w3m /usr/local/bin
 
 #pine-4.64
 cd /home/$user/UBSI_patch
-#wget ftp://ftp.cac.washington.edu/pine/pine.tar.gz
+if [ ! -f ./pine.tar.gz ]; then
+		wget https://www.mirrorservice.org/sites/ftp.cac.washington.edu/pine/pine.tar.gz
+fi
 tar xzvf pine.tar.gz
 cd pine4.64 && patch -p1 < ../pine4.64.patch
 ./build slx SSLDIR=/lib/x86_64-linux-gnu SSLINCLUDE=/usr/include/openssl
@@ -75,7 +87,9 @@ sudo cp -f ./pine/pine /usr/local/bin
 
 #Cherokee-1.2.1:
 cd /home/$user/UBSI_patch
-#wget http://www.gtlib.gatech.edu/pub/cherokee/1.2/1.2.1/cherokee-1.2.1.tar.gz
+if [ ! -f ./cherokee-1.2.1.tar.gz ]; then
+		wget http://repository.timesys.com/buildsources/c/cherokee/cherokee-1.2.1/cherokee-1.2.1.tar.gz
+fi
 tar xzvf cherokee-1.2.1.tar.gz
 cd cherokee-1.2.1 && patch -p1 < ../cherokee-1.2.1.patch
 ./configure && make
@@ -87,7 +101,9 @@ sudo cp -f /usr/local/sbin/cherokee-worker /usr/local/bin
 
 #Yafc-1.1.1: 
 cd /home/$user/UBSI_patch
-#wget http://downloads.sourceforge.net/project/yafc/yafc/yafc-1.1.1/yafc-1.1.1.tar.gz
+if [ ! -f ./yafc-1.1.1.tar.gz ]; then
+		wget http://downloads.sourceforge.net/project/yafc/yafc/yafc-1.1.1/yafc-1.1.1.tar.gz
+fi
 tar xzvf yafc-1.1.1.tar.gz
 cd yafc-1.1.1 && patch -p1 < ../yafc-1.1.1.patch
 ./configure && make
@@ -96,7 +112,9 @@ sudo cp -f src/yafc /usr/local/bin
 
 #Transmission-2.60
 cd /home/$user/UBSI_patch
-#wget https://download.transmissionbt.com/files/transmission-2.60.tar.bz2
+if [ ! -f ./transmission-2.60.tar.bz2 ]; then
+		wget https://github.com/transmission/transmission-releases/raw/master/transmission-2.60.tar.bz2
+fi
 bunzip2 transmission-2.60.tar.bz2 && tar xvf transmission-2.60.tar
 cd transmission-2.60 && patch -p1 < ../transmission-2.60.patch
 ./configure && make
@@ -106,7 +124,9 @@ sudo cp -f ./cli/transmission-cli /usr/local/bin
 
 #Bash-4.3.30: 
 cd /home/$user/UBSI_patch
-#wget http://ftp.gnu.org/gnu/bash/bash-4.3.30.tar.gz
+if [ ! -f ./bash-4.3.30.tar.gz ]; then
+		wget http://ftp.gnu.org/gnu/bash/bash-4.3.30.tar.gz
+fi
 tar xzvf bash-4.3.30.tar.gz
 cd bash-4.3.30 && patch -p1 < ../bash-4.3.30.patch
 ./configure && make
@@ -114,7 +134,9 @@ sudo cp -f ./bash /usr/local/bin
 
 #MidnightCommand-4.6.1: 
 cd /home/$user/UBSI_patch
-#wget http://ftp.midnight-commander.org/mc-4.6.1.tar.gz
+if [ ! -f ./mc-4.6.1.tar.gz ]; then
+		wget http://ftp.midnight-commander.org/mc-4.6.1.tar.gz
+fi
 tar xzvf mc-4.6.1.tar.gz
 cd mc-4.6.1 && patch -p1 < ../mc-4.6.1.patch
 ./configure && make
@@ -122,7 +144,9 @@ sudo cp -f ./src/mc /usr/local/bin
 
 #Sshd-7.2p2: 
 cd /home/$user/UBSI_patch
-#wget http://ftp.vim.org/security/OpenSSH/openssh-7.2p2.tar.gz
+if [ ! -f ./openssh-7.2p2.tar.gz ]; then
+		wget http://ftp.vim.org/security/OpenSSH/openssh-7.2p2.tar.gz
+fi
 tar xzvf openssh-7.2p2.tar.gz
 cd openssh-7.2p2 && patch -p1 < ../openssh-7.2p2.patch
 ./configure && make
@@ -133,7 +157,9 @@ sudo cp -f /usr/local/bin/ssh /usr/local/bin
 
 #Sendmail-8.15.2:
 cd /home/$user/UBSI_patch
-#wget ftp://ftp.sendmail.org/pub/sendmail/sendmail.8.15.2.tar.gz
+if [ ! -f ./sendmail.8.15.2.tar.gz ]; then
+		wget ftp://ftp.sendmail.org/pub/sendmail/sendmail.8.15.2.tar.gz
+fi
 tar xzvf sendmail.8.15.2.tar.gz
 cd sendmail-8.15.2 
 chmod 644 devtools/OS/Linux
@@ -161,7 +187,9 @@ cd /home/$user/UBSI_patch
 #sudo ln -f -s /usr/local/firefox-42.0/firefox-build/dist/bin/firefox /usr/local/bin/
 
 
-wget https://ftp.mozilla.org/pub/firefox/releases/54.0.1/source/firefox-54.0.1.source.tar.xz
+if [ ! -f ./firefox-54.0.1.source.tar.xz ]; then
+		wget https://ftp.mozilla.org/pub/firefox/releases/54.0.1/source/firefox-54.0.1.source.tar.xz
+fi
 tar xf firefox-54.0.1.source.tar.xz
 mkdir /home/$user/.mozbuild
 cd firefox-54.0.1 && patch -p1 < ../firefox-54.0.1.patch
